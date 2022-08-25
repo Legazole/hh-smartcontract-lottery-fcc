0c0ac7acb6b48152520c8b0f22e2f7e3e45a9320
Legazole <LegazoleNFT@gmail.com>
2022-08-25 14:54:33 +0200

    started getRandomWords chainlink function

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -11,16 +11,30 @@ pragma solidity ^0.8.8;
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract Raffle is VRFConsumerBaseV2 {
     // State variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
 
     //events
     event RaffleEnter(address indexedPlayer);
 
-    constructor(uint256 _entraceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 _entraceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = _entraceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
     }
 
     function enterRaffle() public payable {
@@ -33,7 +47,15 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    function pickRandomWinner() public {}
+    //Finish of request function
+    function requestRandomWinner() external {
+        i_vrfCoordinator.requestRandomWords(i_gasLane,i_subscriptionId,,,)
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
